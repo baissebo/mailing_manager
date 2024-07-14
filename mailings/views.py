3898741a44dbf6af3e49def03584e526a820dd88
Alexander Gedz <gedz.buh@yandex.ru>
2024-07-14 12:13:50 +0300

    фикс base

--- mailings/views.py
+++ mailings/views.py
@@ -6,12 +6,10 @@ from mailings.models import Mailing
 
 class MailingListView(ListView):
     model = Mailing
-    paginate_by = 10
-    template_name = 'mailings/mailing_list.html'
-    ordering = ['-created_at']
+    paginate_by = 3
 
     def get_queryset(self):
-        return Mailing.objects.all().order_by('created_at')
+        return Mailing.objects.all().order_by('-created_at')
 
 
 class MailingDetailView(DetailView):
